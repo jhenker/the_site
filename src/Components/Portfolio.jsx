60e398a70e61daff2a381ebe51b43a2f78bf6d9e
Jack Henker <jhenker@uvic.ca>
2024-07-29 04:02:24 +0000

    Sheep picture of mine instead of online pic

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/lake-8763490_1280.jpg";
+import image from "../images/sheep.jpg";
 
-const imageAltText = "The lake with boats.";
+const imageAltText = "A sheep in Scotland.";
 
 /**
  * Project list
